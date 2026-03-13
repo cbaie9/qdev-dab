aca7bc9f82e9bf67b4616bbf2b2742efdafe20f8
cb9 <cbaie9@proton.me>
2026-03-13 15:43:21 +0100

    Adding test for checkWithdrawal

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,8 +2,14 @@ import unittest
 from dab.account import *
 
 class DABUnitTests(unittest.TestCase):
-  def test_checkWithdrawal(self):
-    self.assertEqual(True, False)
+  def test_checkWithdrawal_A1(self):
+    self.assertEqual(checkWithdrawal(600, 200), True)
+  def test_checkWithdrawal_A2(self):
+    self.assertEqual(checkWithdrawal(600, 600), False)
+  def test_checkWithdrawal_A3(self):
+    self.assertEqual(checkWithdrawal(300, 450), False)
+  def test_checkWithdrawal_A4(self):
+    self.assertEqual(checkWithdrawal(600, -3), False)
 
 if __name__ == '__main__':
   unittest.main()
